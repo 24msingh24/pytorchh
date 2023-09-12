556167acb6dd2478cdb05ab45400313747b6c4cb
Manandeep Singh <manandeep1500@gmail.com>
2023-09-13 03:55:58 +1000

    .

--- slurm.sh
+++ slurm.sh
@@ -1,8 +1,12 @@
-#!/bin/bash 
-#SBATCH --job-name=task2
-#SBATCH --mail-type=All
-#SBATCH --mail-user=manandeep.singh@uqconnect.edu.au
-#SBATCH --partition=vgpu
+#!/bin/bash
+#SBATCH --job-name=test
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32G
+#SBATCH --partition=vgpu20
 #SBATCH --gres=gpu:1
 
-python ~/pytorchh/task2.py
+source activate /home/Student/s4823925/manan  
+
+/home/Student/s4823925/manan/bin/python /home/Student/s4823925/pytorchh/task2.py
