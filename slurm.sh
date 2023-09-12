4589767e45563a496ddcb0ef722b8be1d6865cd3
Manandeep Singh <manandeep1500@gmail.com>
2023-09-13 04:12:00 +1000

    .

--- slurm.sh
+++ slurm.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=test
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=8G
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=32G
 #SBATCH --partition=vgpu
 #SBATCH --gres=gpu:1
 
-source activate /home/Student/s4823925/manan  
+source /home/Student/s4823925/miniconda3/bin/activate /home/Student/s4823925/manan
+python task2.py
 
-/home/Student/s4823925/manan/bin/python /home/Student/s4823925/pytorchh/task2.py
