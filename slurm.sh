edb219da4d3071b5d13d14d26adc5f5496c187e0
Manandeep Singh <manandeep1500@gmail.com>
2023-09-13 04:19:31 +1000

    .

--- slurm.sh
+++ slurm.sh
@@ -3,8 +3,8 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=8G
-#SBATCH --partition=vgpu
+#SBATCH --mem=4G
+#SBATCH --partition=vgpu20
 #SBATCH --gres=gpu:1
 
 source /home/Student/s4823925/miniconda3/bin/activate /home/Student/s4823925/manan
