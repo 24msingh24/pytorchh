321f1c517e0f8fefd3f9485d74aa8ad0a05c7ed9
Manandeep Singh <manandeep1500@gmail.com>
2023-09-13 04:54:16 +1000

    .

--- slurm.sh
+++ slurm.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
-#SBATCH --job-name=test
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=8G
-#SBATCH --partition=p100
+#!/bin/bash 
+#SBATCH --job-name=manann
+#SBATCH --mail-type=All
+#SBATCH --mail-user=manandeep.singh@uqconnect.edu.au
+#SBATCH --partition=vgpu
 #SBATCH --gres=gpu:1
 
-source /home/Student/s4823925/miniconda3/bin/activate /home/Student/s4823925/manan
-python task2.py
-
+python ~/pytorchh/task2.py
